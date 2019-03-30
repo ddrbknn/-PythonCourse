98f8d710221bc3951156f099a0868826c45f82f5
ddrbknn <48336841+ddrbknn@users.noreply.github.com>
2019-03-30 09:53:01 +0300

    Update hw_C7.py

--- hw_C7.py
+++ hw_C7.py
@@ -7,17 +7,17 @@ b = input()
 a = [float(symbol) for symbol in b.split()]
 x1 = a[0]
 x2 = a[1]
-H = (x2-x1)/(100*(x2-x1+2))#весы
-y1 = abs(sin(x1)*H)
-y2 = abs(sin(x2)*H)
+#H = (x2-x1)/(100*(x2-x1+2))#весы
+y1 = abs(sin(x1)*(ceil(x1)-x1))
+y2 = abs(sin(x2)*(floor(x2)-x1))
 S = y1 + y2
-
 #for x in range (ceil(x1), floor(x2)):
 #for x in range (x1, x2, 0.):
 x = x1
-while x < x2:
-    x = x + 0.01
-    y = abs(sin(x)*H)
+step = 0.01
+for x in range (ceil(x1), floor(x2)):
+    x = x + step
+    y = abs(sin(x)*step)
     S = S + y
 print ('Методом прямоугольнико      ',S)
 
